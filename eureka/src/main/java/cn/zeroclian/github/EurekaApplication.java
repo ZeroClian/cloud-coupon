d0d26178f58ac126307063cb05a6be091a13140e
ZeroClian <zeroycl@163.com>
2021-02-25 19:40:32 +0800

    Eureka注册中心

---
+++ eureka/src/main/java/cn/zeroclian/github/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.zeroclian.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Desciption  Eureka 启动入口
+ * @Author ZeroClian
+ * @Date 2021-02-25-19:05
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
