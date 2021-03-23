a11b3c257279a76347afd817065f7f58dab0c025
ZeroClian <zeroycl@163.com>
2021-03-23 15:49:18 +0800

    创建权限探测器模块及初始化配置

---
+++ service/permission-detector/src/main/java/cn/zeroclian/github/PermissionSDKConfig.java
@@ -0,0 +1,14 @@
+package cn.zeroclian.github;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Desciption  引入 PermissionSDK 所需的配置类
+ * @Author ZeroClian
+ * @Date 2021-03-23-15:46
+ */
+@Configuration
+@EnableFeignClients
+public class PermissionSDKConfig {
+}
